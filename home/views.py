b5882ff29b6a0591823eb4c64b01859815299fc2
Omar Faruk <Omarfaruk885@gmail.com>
2023-05-10 12:19:41 +0600

    Django-React Integrated

--- home/views.py
+++ home/views.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 from rest_framework import status
 
 def display(request):
-    return HttpResponse("Hello")
+    return render(request,'index.html')
 
 class HelloWorld(APIView):
     def get(self, request):
